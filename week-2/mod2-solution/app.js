682a63fdaa20c80cdc87f9f6abd28f19f97d7f3b
manish-giri <manish.giri.me@gmail.com>
2017-11-05 10:08:11 -0500

    Solve Week 2 Assignment

---
+++ week-2/mod2-solution/app.js
@@ -0,0 +1,44 @@
+(function() {
+
+    var ShoppingListCheckOff = angular.module('ShoppingListCheckOff', []);
+
+    ShoppingListCheckOff.service('ShoppingListCheckOffService', function() {
+        let toBuy = [
+            { name: "cookies", quantity: 10 },
+            { name: "chips", quantity: 3 },
+            { name: "water", quantity: 5 },
+            { name: "milk", quantity: 2 },
+            { name: "pepsi", quantity: 12 }
+        ];
+
+        let bought = [];
+
+        this.buyItem = function(index) {
+            let boughtItem = toBuy.splice(index, 1);
+            // console.log(boughtItem[0]);
+            bought.push(boughtItem[0]);
+        }
+
+        this.toBuyItems = function() {
+            return toBuy;
+        }
+
+        this.boughtItems = function() {
+            return bought;
+        }
+    });
+
+    ShoppingListCheckOff.controller('ToBuyController', ['$scope', 'ShoppingListCheckOffService', function($scope, ShoppingListCheckOffService) {
+        $scope.buyItems = ShoppingListCheckOffService.toBuyItems();
+
+        $scope.buyThisItem = function(idx) {
+            ShoppingListCheckOffService.buyItem(idx);
+        };
+    }]);
+
+    
+    ShoppingListCheckOff.controller('AlreadyBoughtController', ['$scope', 'ShoppingListCheckOffService', function($scope, ShoppingListCheckOffService) {
+        $scope.boughtItems = ShoppingListCheckOffService.boughtItems();
+    }]);
+
+})();
